df843770e80412d87f27fbaa13bde8c7ec9fc202
Mfitumukiza Peter <mfitep6@gmail.com>
2022-04-30 08:22:18 +0200

    migrating

--- js/home.js
+++ js/home.js
@@ -3,8 +3,6 @@ const closeBtn = document.querySelector('.fa-xmark');
 const nav = document.querySelector('nav');
 const logoImg = document.querySelector('.logo');
 
-
-
 dashboardBars.addEventListener("click", ()=>{
     nav.classList.add('show');
     logoImg.src = "../images/black-logo.png";
